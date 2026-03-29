734f76e0659666de35923c8f755ce0e167e85b34
Krunal Varma <varmakrunal64@gmail.com>
2026-03-29 10:56:56 +0530

    Update Firebase config with measurementId
    Added measurementId to Firebase configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
 // Your Firebase configuration
 // Replace with your own project's config
 const firebaseConfig = {
-    apiKey: "AIzaSyAtGGnQBptMwGJ74S0qzm8WZDotoPUDqaA",
-    authDomain: "sinior-to-junior.firebaseapp.com",
-    projectId: "sinior-to-junior",
-    storageBucket:"sinior-to-junior.firebasestorage.app",
-    messagingSenderId:"984394391505",
-    appId: "1:984394391505:web:fa4e0e0f836aac3128caf0"
-};
+  apiKey: "AIzaSyAtGGnQBptMwGJ74S0qzm8WZDotoPUDqaA",
+  authDomain: "sinior-to-junior.firebaseapp.com",
+  projectId: "sinior-to-junior",
+  storageBucket: "sinior-to-junior.firebasestorage.app",
+  messagingSenderId: "984394391505",
+  appId: "1:984394391505:web:fa4e0e0f836aac3128caf0",
+  measurementId: "G-84GE37REHY"
+};;
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
