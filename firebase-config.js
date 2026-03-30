93b19c971036d3c3f769994f560d5917005db69d
Krunal Varma <varmakrunal64@gmail.com>
2026-03-30 15:26:08 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,18 +2,17 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCIqumBlbeRHFAWc6NtEFgVhnAMrr6X80c",
   authDomain: "j-to-s-51c86.firebaseapp.com",
-  projectId: "j-to-s-51c86",                          // ✅ comma add kiya
+  projectId: "j-to-s-51c86",
   storageBucket: "j-to-s-51c86.firebasestorage.app",
   messagingSenderId: "376438283237",
   appId: "1:376438283237:web:11ca777eec9a1747936d0c",
   measurementId: "G-1MQCVVTS6P"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();                          // ✅ pehle define karo
+const auth = firebase.auth();
 const db = firebase.firestore();
-const googleProvider = new firebase.auth.GoogleAuthProvider(); // ✅ Google login ke liye
+const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-console.log("Firebase initialized!", auth);            // ✅ ab use karo
+console.log("Firebase initialized!", auth);
