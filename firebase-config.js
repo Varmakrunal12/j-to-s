79fc49a8767df8a4fbfe68e259706148c83f995c
Krunal Varma <varmakrunal64@gmail.com>
2026-03-30 15:04:22 +0530

    Modify Firebase config for new project
    Updated Firebase configuration with new project details.

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,13 @@
 console.log("Firebase initialized!", auth);
 // Replace with your own project's config
 const firebaseConfig = {
-  apiKey: "AIzaSyAtGGnQBptMwGJ74S0qzm8WZDotoPUDqaA",
-  authDomain: "sinior-to-junior.firebaseapp.com",
-  projectId: "sinior-to-junior",
-  storageBucket: "sinior-to-junior.firebasestorage.app",
-  messagingSenderId: "984394391505",
-  appId: "1:984394391505:web:fa4e0e0f836aac3128caf0",
-  measurementId: "G-84GE37REHY"
+  apiKey:"AIzaSyCIqumBlbeRHFAWc6NtEFgVhnAMrr6X80c",
+  authDomain:  "j-to-s-51c86.firebaseapp.com",
+  projectId: "j-to-s-51c86"
+  storageBucket: "j-to-s-51c86.firebasestorage.app",
+  messagingSenderId: "376438283237",
+  appId: "1:376438283237:web:11ca777eec9a1747936d0c",
+  measurementId: "G-1MQCVVTS6P"
 };;
 
 // Initialize Firebase
