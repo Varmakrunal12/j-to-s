553f3ba459365f37dfd40a61fd08a166ad21babd
Krunal Varma <varmakrunal64@gmail.com>
2026-03-30 15:10:04 +0530

    Refactor Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
 // Your Firebase configuration
-console.log("Firebase initialized!", auth);
 // Replace with your own project's config
 const firebaseConfig = {
   apiKey:"AIzaSyCIqumBlbeRHFAWc6NtEFgVhnAMrr6X80c",
   authDomain:  "j-to-s-51c86.firebaseapp.com",
-  projectId: "j-to-s-51c86"
+  projectId: "j-to-s-51c86",
   storageBucket: "j-to-s-51c86.firebasestorage.app",
   messagingSenderId: "376438283237",
   appId: "1:376438283237:web:11ca777eec9a1747936d0c",
